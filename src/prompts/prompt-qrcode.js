aa8b3e6ab3ab1bec932849c55f9dce035be9a664
Alex Sandro <alex@creapix.com.br>
2025-03-31 11:06:00 -0300

    feat: add qrcode generate

---
+++ src/prompts/prompt-qrcode.js
@@ -0,0 +1,21 @@
+import chalk from "chalk";
+
+
+const promptQRCode = [
+    {
+        name: "link",
+        description: chalk.yellow("Digite o link que deseja gerar o QR Code"),
+    },
+{
+    name: "type",
+    description: chalk.yellow("Escolha o tipo de QR Code [1] - Normal | [2] - Terminal"),
+    pattern: /^[1-2]+$/,
+    message: chalk.red.italic("Apenas números 1 ou 2"),
+    required: true,
+},
+
+
+];
+
+export default promptQRCode;1
+
